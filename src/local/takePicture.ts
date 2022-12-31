e27d94de1ef18a26d36bda019ac04da4163a3aef
AnthonyLzq <sluzquinosa@uni.pe>
2022-12-31 12:16:41 -0500

    feat: implemented capability to send the photo using the mqtt protocol

--- src/local/takePicture.ts
+++ src/local/takePicture.ts
@@ -5,11 +5,18 @@ import { getTimestamp } from 'utils'
 const takePicture = async () => {
   const timestamp = getTimestamp()
 
-  await capture({
-    location: resolve(__dirname, '..', '..', 'media', `photo_${timestamp}.png`),
+  return await capture({
+    location: resolve(
+      __dirname,
+      '..',
+      '..',
+      'media',
+      `photo_test_${timestamp}.png`
+    ),
     options: {
       output: 'png'
-    }
+    },
+    returnType: 'buffer'
   })
 }
 
---
+++ src/mqtt/server.ts
@@ -0,0 +1,42 @@
+import mqtt from 'mqtt'
+import debug from 'debug'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __mqttClient__: mqtt.MqttClient
+}
+
+const options: mqtt.IClientOptions = {
+  port: process.env.MQTT_PORT ? parseInt(process.env.MQTT_PORT) : 0,
+  host: process.env.MQTT_HOST,
+  protocol: 'mqtts',
+  keepalive: 0,
+  username: process.env.MQTT_USER,
+  password: process.env.MQTT_PASS
+}
+const namespace = 'DoorCloud:Mqtt:Server'
+const debugMessage = 'Connected to mqtt server'
+
+const getClient = () => {
+  if (!global.__mqttClient__) {
+    global.__mqttClient__ = mqtt.connect(options)
+    global.__mqttClient__.on('connect', () => {
+      const clientDebug = debug(namespace)
+
+      clientDebug(debugMessage)
+    })
+  }
+
+  return global.__mqttClient__
+}
+
+const mqttConnection = () => ({
+  start: async () => {
+    return global.__mqttClient__
+  },
+  stop: async () => {
+    getClient().end()
+  }
+})
+
+export { getClient, mqttConnection, namespace, debugMessage }
